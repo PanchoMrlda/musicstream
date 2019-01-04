2a1403d75880c22776b07b05735d6815e2394cc9
PanchoMrlda <fco.moraledac@gmail.com>
2019-01-05 00:15:55 +0100

    Fixing access to album after chaging relationships to many_to_many in SongsController

--- app/Http/Controllers/SongsController.php
+++ app/Http/Controllers/SongsController.php
@@ -38,6 +38,13 @@ class SongsController extends Controller
     $albumName = explode("/", $songData)[1];
     $artistName = explode("/", $songData)[0];
     $artist = Artist::where('name', '=', $artistName)->get()->first();
+    Album::join('album_artists', 'albums.id', '=', 'album_artists.album_id')
+         ->where([
+          ['name', '=', $albumName],
+          ['artist_id', '=', $artist->id]
+         ])
+         ->get()
+         ->first();
     $album = Album::where([
       ['name', '=', $albumName],
       ['artist_id', '=', $artist->id]
