2930f439a175290bdff85e2dcb891a93cd24e9e3
PanchoMrlda <fco.moraledac@gmail.com>
2019-01-06 12:02:13 +0100

    Adding search modal to music view

--- resources/views/music.blade.php
+++ resources/views/music.blade.php
@@ -58,3 +58,24 @@
   </div>
   @include('layouts.songs')
 @endsection
+
+<!-- Modal -->
+<div class="modal fade" id="search-modal" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-center">Search for an artist</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Yolo
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Search</button>
+      </div>
+    </div>
+  </div>
+</div>
