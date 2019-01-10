62debbe89ba6e38947503e36274d8b41872112b7
PanchoMrlda <fco.moraledac@gmail.com>
2019-01-10 23:32:37 +0100

    Avoid href to home page in sideMenu view

--- resources/views/layouts/sideMenu.blade.php
+++ resources/views/layouts/sideMenu.blade.php
@@ -2,7 +2,7 @@
   <a href="#" class="close"><i class="fa fa-angle-left"></i></a>
   <ul>
     <li><a href="#" class="side-search"><i class="fa fa-search fa-fw"></i> Search</a></li>
-    <li class="active"><a href="/" class="side-currently-playing"><i class="fa fa-headphones fa-fw"></i> Currently Playing</a></li>
+    <li class="active"><a href="#" class="side-currently-playing"><i class="fa fa-headphones fa-fw"></i> Currently Playing</a></li>
     <li><a href="/topCharts" class="side-popular-music"><i class="fa fa-list-ol fa-fw"></i> Popular Music</a></li>
     <li><a href="#" class="side-your-playlist"><i class="fa fa-music fa-fw"></i> Your Playlists</a></li>
     <li><a href="#" class="side-your-profile"><i class="fa fa-user fa-fw"></i> Your Profile</a></li>
