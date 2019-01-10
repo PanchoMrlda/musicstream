982969e3f933899e2499aeec6fe742f1471415dd
PanchoMrlda <fco.moraledac@gmail.com>
2019-01-10 23:28:23 +0100

    Adding request to get songs from artist or from album in SongsController

--- app/Http/Controllers/SongsController.php
+++ app/Http/Controllers/SongsController.php
@@ -53,4 +53,55 @@ class SongsController extends Controller
     ])->get()->first();
     $song->incPlays();
   }
+
+  public function artistSongs(Request $request)
+  {
+    $artistName = $request->name;
+    if ($artistName) {
+      $htmlSongs = "";
+      $htmlAlbums = "";
+      $artist = Artist::where('name', '=', $artistName)->get()->first();
+      $albums = $artist->albums;
+      $songs = [];
+      foreach ($albums as $album) {
+        $songs[] = $album->songs->where('artist_id', '=', $artist->id);
+      }
+      $songs = array_flatten($songs);
+      foreach ($songs as $index => $song) {
+        $htmlAlbums .= "<li><img src='".$song->getImageUrl()."' alt='' /></li>";
+        if ($index == 0) {
+          $htmlSongs .= "<audio class='audio active' artist='".$song->artist->name."' src='".$song->getAudioUrl()."' type='audio/mpeg'>Your browser does not support the audio tag!</audio>";
+        } else {
+          $htmlSongs .= "<audio class='audio' artist='".$song->artist->name."' src='".$song->getAudioUrl()."' type='audio/mpeg'>Your browser does not support the audio tag!</audio>";
+        }
+      }
+      $result = [$htmlAlbums, $htmlSongs];
+    } else {
+      $result = [];
+    }
+    return response()->json($result);
+  }
+
+  public function albumSongs(Request $request)
+  {
+    $albumName = $request->name;
+    if ($albumName) {
+      $htmlSongs = "";
+      $htmlAlbum = "";
+      $album = Album::where('name', '=', $albumName)->get()->first();
+      $songs = $album->songs;
+      $htmlAlbum = "<li><img src='".$songs->first()->getImageUrl()."' alt='' /></li>";
+      foreach ($songs as $index => $song) {
+        if ($index == 0) {
+          $htmlSongs .= "<audio class='audio active' album='".$song->album->name."' src='".$song->getAudioUrl()."' type='audio/mpeg'>Your browser does not support the audio tag!</audio>";
+        } else {
+          $htmlSongs .= "<audio class='audio' album='".$song->album->name."' src='".$song->getAudioUrl()."' type='audio/mpeg'>Your browser does not support the audio tag!</audio>";
+        }
+      }
+      $result = [$htmlAlbum, $htmlSongs];
+    } else {
+      $result = [];
+    }
+    return response()->json($result);
+  }
 }
