b5c181f591f82ee96034e3e9751a32152f6a2b68
PanchoMrlda <fco.moraledac@gmail.com>
2019-01-10 23:33:04 +0100

    Implementing search menu view

---
+++ resources/views/layouts/searchMenu.blade.php
@@ -0,0 +1,15 @@
+<div class="side-menu search-menu">
+  <div class="search-menu-header">
+    <a href="#" class="close"><i class="fa fa-angle-left"></i></a>
+  </div>
+  <div class="search-menu-body vertical-center">
+    <label class="text-center">Search for an artist</label>
+    <input id="search-artist" class="center-block">
+    <label class="text-center">Search for an album</label>
+    <input id="search-album" class="center-block">
+  </div>
+  <div class="search-menu-footer">
+    <button type="button" class="btn btn-secondary">Close</button>
+    <button type="button" class="btn btn-primary">Search</button>
+  </div>
+</div>
