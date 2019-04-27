55170401d188e4000448ed05f28a7f35f6d63580
PanchoMrlda <fco.moraledac@gmail.com>
2019-04-27 18:29:13 +0200

    Showing static playlists in playlists view

--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -25,87 +25,78 @@
       <img class="profile-image" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb737fc148f14000d6ec9dd/img/now-playing-profile-image@2x.png>
     </figure>
   </header>
-    <main class="main">
-      <nav class="main-nav">
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-        <div class="main-nav__item"></div>
-      </nav>
-      <section class="content">
-        <div class="playing">
-          <div class="info">
-            <span class="song">Inis Mona</span>
-            <br>
-            <span class="artist">Eluveitie</span>
-          </div>
-          <div class="button">
-            <span class="far play">&#xf144;</span>
-            <span class="far pause" style="display:none">&#xf28b;</span>
-          </div>
+  <main class="main">
+    <nav class="main-nav">
+      <div class="main-nav__item">
+        Favourites
+      </div>
+      <div class="main-nav__item">
+        Rock
+      </div>
+      <div class="main-nav__item">
+        Folk
+      </div>
+      <div class="main-nav__item">
+        Country
+      </div>
+      <div class="main-nav__item">
+        Pop
+      </div>
+    </nav>
+    <section class="content">
+      <div class="playing">
+        <div class="info">
+          <span class="song">Inis Mona</span>
+          <br>
+          <span class="artist">Eluveitie</span>
         </div>
-        <div class="songs">
-          @if(count($songs)> 0)
-          @foreach($songs as $song)
-          <div class="song-container" onclick="playAudio" src="{{$song->getAudioUrl()}}">
-            <img class=divider src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb726d4c5322c000a357f28/img/all-tracks-line-4-copy-5.png>
-            <br>
-            <span class="name">{{$song->name}}</span>
-            <br>
-            <span class="artist">{{$song->artist->name}}</span>
-          </div>
-          @endforeach
-          @endif
+        <div class="button">
+          <span class="far play">&#xf144;</span>
+          <span class="far pause" style="display:none">&#xf28b;</span>
         </div>
+      </div>
+      <div class="songs">
+        @if(count($songs)> 0)
+        @foreach($songs as $song)
+        <div class="song-container" onclick="playAudio" src="{{$song->getAudioUrl()}}">
+          <img class=divider src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb726d4c5322c000a357f28/img/all-tracks-line-4-copy-5.png>
+          <br>
+          <span class="name">{{$song->name}}</span>
+          <br>
+          <span class="artist">{{$song->artist->name}}</span>
         </div>
-      </section>
-    </main>
-    <footer class="footer">
-      <a class="anchor" href="topHits">
+        @endforeach
+        @endif
+      </div>
+      </div>
+    </section>
+  </main>
+  <footer class="footer">
+    <a class="anchor" href="topHits">
+    <div class="icon">
+        <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb726d4c5322c000a357f28/img/all-tracks-shape@2x.png>
+      </div>
+      <span class="icon-caption">Top Hits</span>
+    </a>
+    <a class="anchor" href="allTracks">
       <div class="icon">
-          <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb726d4c5322c000a357f28/img/all-tracks-shape@2x.png>
-        </div>
-        <span class="icon-caption">Top Hits</span>
-      </a>
-      <a class="anchor" href="allTracks">
-        <div class="icon">
-          <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cc1b5f9dc4425000aa8c472/img/playlist-combined-shape@2x.png>
-        </div>
-        <span class="icon-caption">All tracks</span>
-      </a>
-      <a class="anchor" href="playlists">
-        <div class="icon">
-          <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb737fc148f14000d6ec9dd/img/now-playing-shape-4@2x.png>
-        </div>
-        <span class="icon-caption">Playlists</span>
-      </a>
-      <a class="anchor" href="#">
-        <div class="icon">
-          <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb726d4c5322c000a357f28/img/all-tracks-shape-5@2x.png>
-        </div>
-        <span class="icon-caption">Search</span>
-      </a>
-    </footer>
+        <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cc1b5f9dc4425000aa8c472/img/playlist-combined-shape@2x.png>
+      </div>
+      <span class="icon-caption">All tracks</span>
+    </a>
+    <a class="anchor" href="playlists">
+      <div class="icon">
+        <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb737fc148f14000d6ec9dd/img/now-playing-shape-4@2x.png>
+      </div>
+      <span class="icon-caption">Playlists</span>
+    </a>
+    <a class="anchor" href="#">
+      <div class="icon">
+        <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb726d4c5322c000a357f28/img/all-tracks-shape-5@2x.png>
+      </div>
+      <span class="icon-caption">Search</span>
+    </a>
+  </footer>
 </body>
 
 </html>
