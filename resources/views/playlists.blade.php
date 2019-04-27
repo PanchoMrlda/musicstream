34b71ecee24b8bd1aa54f655ca3f151ad93eddbe
PanchoMrlda <fco.moraledac@gmail.com>
2019-04-27 15:51:57 +0200

    Implementing footer in playlists view

--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" media="screen" href="css/music-views.css">
   <script src="main.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
 
 <body class="body">
@@ -79,7 +80,32 @@
         </div>
       </section>
     </main>
-    <footer class="footer">Footer</footer>
+    <footer class="footer">
+      <a class="anchor" href="topHits">
+      <div class="icon">
+          <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb726d4c5322c000a357f28/img/all-tracks-shape@2x.png>
+        </div>
+        <span class="icon-caption">Top Hits</span>
+      </a>
+      <a class="anchor" href="allTracks">
+        <div class="icon">
+          <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cc1b5f9dc4425000aa8c472/img/playlist-combined-shape@2x.png>
+        </div>
+        <span class="icon-caption">All tracks</span>
+      </a>
+      <a class="anchor" href="playlists">
+        <div class="icon">
+          <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb737fc148f14000d6ec9dd/img/now-playing-shape-4@2x.png>
+        </div>
+        <span class="icon-caption">Playlists</span>
+      </a>
+      <a class="anchor" href="#">
+        <div class="icon">
+          <img class="shape" src=https://anima-uploads.s3.amazonaws.com/projects/5c63fb77bc2e39000dda1cf8/releases/5cb726d4c5322c000a357f28/img/all-tracks-shape-5@2x.png>
+        </div>
+        <span class="icon-caption">Search</span>
+      </a>
+    </footer>
 </body>
 
 </html>
