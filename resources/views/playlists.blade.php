917762b6c3e62ad3e846aa6903969e104959b2d9
PanchoMrlda <fco.moraledac@gmail.com>
2019-05-02 13:14:30 +0200

    Adding song id info to allTracks and playlists views

--- resources/views/playlists.blade.php
+++ resources/views/playlists.blade.php
@@ -62,7 +62,7 @@
       <div class="songs">
         @if(count($songs)> 0)
         @foreach($songs as $song)
-        <div class="song-container" onclick="initAudio(this)" src="{{$song->getAudioUrl()}}" data-song="{{$song->name}}" data-artist="{{$song->artist->name}}">
+        <div class="song-container" onclick="initAudio(this)" src="{{$song->getAudioUrl()}}" data-song="{{$song->name}}" data-artist="{{$song->artist->name}}" data-id="{{$song->id}}">
           <div>
             <p class="name">{{$song->name}}</p>
             <p class="artist">{{$song->artist->name}}</p>
