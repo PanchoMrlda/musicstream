c27fd5695ed343a843fe53f780f233f27dd0c6ec
PanchoMrlda <fco.moraledac@gmail.com>
2019-05-11 11:12:59 +0200

    Adding music views to MusicController

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -11,7 +11,26 @@ class MusicController extends Controller
   {
     $songs = Song::all();
 
+    return view('allTracks')->with('songs', $songs);
     return view('music')->with('songs', $songs);
+  }
+
+  public function allTracks()
+  {
+    $songs = Song::all();
+    return view('allTracks')->with('songs', $songs);
+  }
+
+  public function playlists()
+  {
+    $songs = Song::all();
+    return view('playlists')->with('songs', $songs);
+  }
+
+  public function topHits()
+  {
+    $songs = Song::orderBy('plays', 'DESC')->take(10)->get();
+    return view('topHits')->with('songs', $songs);
   }
 
   public function topCharts()
