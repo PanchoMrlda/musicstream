47be0c0507cd396b5a2e0f8f872a701e6d65798d
PanchoMrlda <fco.moraledac@gmail.com>
2019-07-10 10:26:13 +0200

    Moving footer in allTracks, playlists, topHits to layout

--- resources/views/topHits.blade.php
+++ resources/views/topHits.blade.php
@@ -68,32 +68,7 @@
       </div>
     </section>
   </main>
-  <footer class="footer">
-    <a class="anchor" onclick="footerGoTo('/topHits', 'Top Hits')">
-      <div class="icon">
-        <img class="shape" src="images/top-hits-white.png">
-      </div>
-      <span class="icon-caption">Top Hits</span>
-    </a>
-    <a class="anchor" onclick="footerGoTo('/allTracks', 'All Tracks')">
-      <div class="icon">
-        <img class="shape" src="images/all-tracks.png">
-      </div>
-      <span class="icon-caption">All Tracks</span>
-    </a>
-    <a class="anchor" onclick="footerGoTo('/playlists', 'Playlists')">
-      <div class="icon">
-        <img class="shape" src="images/playlists.png">
-      </div>
-      <span class="icon-caption">Playlists</span>
-    </a>
-    <a class="anchor" href="#">
-      <div class="icon">
-        <img class="shape" src="images/search.png">
-      </div>
-      <span class="icon-caption">Search</span>
-    </a>
-  </footer>
+  @include('layouts.footer')
 </body>
 
 </html>
