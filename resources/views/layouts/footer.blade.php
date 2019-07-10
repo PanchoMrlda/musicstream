a0162483438480e0e949416f5147aa6bf7604705
PanchoMrlda <fco.moraledac@gmail.com>
2019-07-10 10:25:36 +0200

    Create footer layout to refactor code

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,26 @@
-<footer>
-  <div class="top">
-    <p>Copyright 2018 &copy; PanchosMusic</p>
-  </div>
+<footer class="footer">
+  <a class="anchor" onclick="footerGoTo('/topHits', 'Top Hits')">
+    <div class="icon">
+      <img class="shape" src="images/top-hits.png">
+    </div>
+    <span class="icon-caption">Top Hits</span>
+  </a>
+  <a class="anchor" onclick="footerGoTo('/allTracks', 'All Tracks')">
+    <div class="icon">
+      <img class="shape" src="images/all-tracks.png">
+    </div>
+    <span class="icon-caption">All Tracks</span>
+  </a>
+  <a class="anchor" onclick="footerGoTo('/playlists', 'Playlists')">
+    <div class="icon">
+      <img class="shape" src="images/playlists.png">
+    </div>
+    <span class="icon-caption">Playlists</span>
+  </a>
+  <a class="anchor" href="#">
+    <div class="icon">
+      <img class="shape" src="images/search.png">
+    </div>
+    <span class="icon-caption">Search</span>
+  </a>
 </footer>
