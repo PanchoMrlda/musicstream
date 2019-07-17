a029b9b20a868e5dbf18cbf4ce84b9ba62fd830a
PanchoMrlda <fco.moraledac@gmail.com>
2019-07-17 17:57:34 +0200

    Updating songs layout

--- resources/views/layouts/songs.blade.php
+++ resources/views/layouts/songs.blade.php
@@ -1,14 +1,13 @@
-@if(count($songs) > 0)
-  @foreach($songs as $index => $song)
-  <?php
-  if ($index == 0) {
-    $active = ' active';
-  } else {
-    $active = '';
-  }  
-  ?>
-  <audio class="audio{{$active}}" artist="{{$song->artist->name}}" src="{{$song->getAudioUrl()}}" type="audio/mpeg"> 
-    Your browser does not support the audio tag!
-  </audio>
+<div class="songs">
+  @if(count($songs)> 0)
+  @foreach($songs as $song)
+  <div class="song-container" onclick="initAudio(this)" src="{{$song->getAudioUrl()}}" data-song="{{$song->name}}" data-artist="{{$song->artist->name}}" data-id="{{$song->id}}">
+    <div>
+      <p class="name">{{$song->name}}</p>
+      <p class="artist">{{$song->artist->name}}</p>
+    </div>
+    <p class="song-duration">{{$song->durationToString()}}</p>
+  </div>
   @endforeach
-@endif
+  @endif
+</div>
