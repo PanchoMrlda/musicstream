f897c5a4d9c7b844d0794b6b15e62ff131f6c3f9
PanchoMrlda <fco.moraledac@gmail.com>
2019-07-22 12:49:55 +0200

    Creating layout for main header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<header class="header">
+  <div class="icon-menu-container">
+    <img class="icon-menu" src=images/icon-menu.png>
+  </div>
+  <div class="title-container">
+    <div class="title">
+      <span></span>
+    </div>
+  </div>
+  <figure class="profile-image-container">
+    <img class="profile-image" src=images/pancho.jpg>
+  </figure>
+</header>
