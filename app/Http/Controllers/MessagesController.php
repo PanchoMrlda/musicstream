faa9847f3b10eadd70d6d0631db0c96bf432d48d
PanchoMrlda <fco.moraledac@gmail.com>
2019-07-31 13:21:04 +0200

    Enabling endpoint to send messages to Slack through MessagesController

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Message;
+use Config\SlackMessageSender;
 
 class MessagesController extends Controller
 {
@@ -23,5 +24,48 @@ class MessagesController extends Controller
     $message->save();
     // Redirect
     return redirect('/')->with('success', 'Message Sent');
+  }
+
+  /**
+   * GET /messages
+   */
+  public function index(Request $request)
+  {
+    return true;
+  }
+
+  /**
+   * GET /messages/:id
+   */
+  public function show($id)
+  {
+    return $id;
+  }
+
+  /**
+   * POST /messages
+   */
+  public function store(Request $request)
+  {
+    $messageToSend = $request->text;
+    $slackNotifier = new SlackMessageSender();
+    $slackNotifier->sendMessage($messageToSend);
+    return $messageToSend;
+  }
+
+  /**
+   * PUT /messages/:id
+   */
+  public function update(Request $request, $id)
+  {
+    return $id;
+  }
+
+  /**
+   * DELETE /messages/:id
+   */
+  public function destroy($id)
+  {
+    return $id;
   }
 }
